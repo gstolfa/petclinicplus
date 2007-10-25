f38341edd3bd8df495fa1311d5a52ec74792c84c
lars.behnke@gmx.net <lars.behnke@gmx.net@e30b4600-443d-0410-98c6-9b3d04295a26>
2007-10-25 10:09:37 +0000

    Integrating Acegi

---
+++ trunk/src/main/java/net/larsbehnke/petclinicplus/PetType.java
@@ -0,0 +1,8 @@
+package net.larsbehnke.petclinicplus;
+
+/**
+ * @author Juergen Hoeller
+ */
+public class PetType extends NamedEntity {
+
+}
---
+++ trunk/src/main/java/net/larsbehnke/petclinicplus/jmx/CallMonitor.java
@@ -0,0 +1,22 @@
+package net.larsbehnke.petclinicplus.jmx;
+
+/**
+ * Simple interface for a call monitor.
+ * To be exposed for management via JMX.
+ * 
+ * @author Rob Harrop
+ * @since 1.2
+ */
+public interface CallMonitor {
+
+	void setEnabled(boolean isEnabled);
+
+	boolean isEnabled();
+
+	void reset();
+
+	int getCallCount();
+
+	long getCallTime();
+
+}
