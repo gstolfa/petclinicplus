647d76ca5a7e90c261a889590a2affc3ec9bd104
lars.behnke@gmx.net <lars.behnke@gmx.net@e30b4600-443d-0410-98c6-9b3d04295a26>
2007-11-05 10:29:31 +0000

    Worked on AuthController

--- trunk/sample-spring-ftl-jpa-jsecurity/src/main/java/net/larsbehnke/petclinicplus/web/AuthController.java
+++ trunk/sample-spring-ftl-jpa-jsecurity/src/main/java/net/larsbehnke/petclinicplus/web/AuthController.java
@@ -8,20 +8,49 @@ import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 public class AuthController extends MultiActionController {
 
+	private String logoutView;
+	private String loginView;
+	private String loginErrorView;
+
+	public String getLogoutView() {
+		return logoutView;
+	}
+
+	public void setLogoutView(String logoutView) {
+		this.logoutView = logoutView;
+	}
+
+	public String getLoginView() {
+		return loginView;
+	}
+
+	public void setLoginView(String loginView) {
+		this.loginView = loginView;
+	}
+
+	public String getLoginErrorView() {
+		return loginErrorView;
+	}
+
+	public void setLoginErrorView(String loginErrorView) {
+		this.loginErrorView = loginErrorView;
+	}
+
 	public ModelAndView logoutHandler(HttpServletRequest request,
 			HttpServletResponse response) {
 		if (request.getSession() != null)
 			request.getSession().invalidate();
-		return new ModelAndView("welcome");
+		return new ModelAndView(getLogoutView());
 	}
 
 	public ModelAndView loginHandler(HttpServletRequest request,
 			HttpServletResponse response) {
-		return new ModelAndView("login");
+		return new ModelAndView(getLoginView());
 	}
 
 	public ModelAndView loginErrorHandler(HttpServletRequest request,
 			HttpServletResponse response) {
-		return new ModelAndView("loginerror");
+	
+		return new ModelAndView(getLoginErrorView());
 	}
 }
